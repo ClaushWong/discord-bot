bbbf9181e758ed283c7e5a99889edd37d723b8c7
Claush Wong <claushwong123@gmail.com>
2023-11-18 18:03:43 +0800

    update-3: change the root path for execution

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+// import discord.js components
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import { onClientReady, onHandleInteraction } from "@src/bot.process";
+
+// create a new Client instance
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds],
+});
+
+// call to make sure it is online on client side (our side)
+client.once(Events.ClientReady, onClientReady);
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on(Events.InteractionCreate, onHandleInteraction);
